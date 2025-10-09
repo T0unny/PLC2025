535ceccda7c28239b806dcc0b45263d08f553b6c
Pedro Ribeiro <pedroantonio.pr4o@gmail.com>
2025-10-09 12:58:57 +0100

    Update TPC3.py

--- TP3/TPC3.py
+++ TP3/TPC3.py
@@ -4,7 +4,7 @@ import re
 
 def tokenize(input_string,linha):
     reconhecidos = []
-    mo = re.finditer(r'(?P<SELECT>SELECT)|(?P<WHERE>WHERE)|(?P<Variavel>\?\w+)|(?P<PA>\{)|(?P<PONTO>\.)|(?P<String>\w*:\w+)|(?P<PF>\})|(?P<SKIP>[ \t])|(?P<NEWLINE>\n)|(?P<ERRO>.)', input_string)
+    mo = re.finditer(r'(?P<SELECT>SELECT|select)|(?P<WHERE>WHERE|where)|(?P<LIMIT>LIMIT)|(?P<Variavel>\?\w+)|(?P<abreviacaoRdf>a)|(?P<PA>\{)|(?P<PONTO>\.)|(?P<PrefixedName>\w*:\w+)|(?P<String>"(\w+ ?)+")|(?P<idoma>@\w+)|(?P<int>\d+)|(?P<PF>\})|(?P<SKIP>( )|(\t)|(#.*\n))|(?P<NEWLINE>\n)|(?P<ERRO>.)', input_string)
     for m in mo:
         dic = m.groupdict()
         if dic['SELECT']:
@@ -13,8 +13,14 @@ def tokenize(input_string,linha):
         elif dic['WHERE']:
             t = ("WHERE", dic['WHERE'], linha, m.span())
     
+        elif dic['LIMIT']:
+            t = ("LIMIT", dic['LIMIT'], linha, m.span())
+    
         elif dic['Variavel']:
             t = ("Variavel", dic['Variavel'], linha, m.span())
+    
+        elif dic['abreviacaoRdf']:
+            t = ("abreviacaoRdf", dic['abreviacaoRdf'], linha, m.span())
     
         elif dic['PA']:
             t = ("PA", dic['PA'], linha, m.span())
@@ -22,8 +28,17 @@ def tokenize(input_string,linha):
         elif dic['PONTO']:
             t = ("PONTO", dic['PONTO'], linha, m.span())
     
+        elif dic['PrefixedName']:
+            t = ("PrefixedName", dic['PrefixedName'], linha, m.span())
+    
         elif dic['String']:
             t = ("String", dic['String'], linha, m.span())
+    
+        elif dic['idoma']:
+            t = ("idoma", dic['idoma'], linha, m.span())
+    
+        elif dic['int']:
+            t = ("int", dic['int'], linha, m.span())
     
         elif dic['PF']:
             t = ("PF", dic['PF'], linha, m.span())
@@ -42,10 +57,9 @@ def tokenize(input_string,linha):
         if not dic['SKIP'] and t[0] != 'UNKNOWN': reconhecidos.append(t)
     return reconhecidos
 
-
-cont = 0
+counter= 0
 for linha in sys.stdin:
-    cont += 1
-    for tok in tokenize(linha,cont):
+    counter+=1
+    for tok in tokenize(linha,counter):
         print(tok)    
 
